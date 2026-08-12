bd7d786a5e265b6cb3a8b717904bf97e4ffe10d4
자유해결사 <khjde1207@gmail.com>
2026-08-12 21:24:43 +0900

    Add Android gamepad device events

--- android/src/main/kotlin/com/example/flutter_gamepad/FlutterGamepadPlugin.kt
+++ android/src/main/kotlin/com/example/flutter_gamepad/FlutterGamepadPlugin.kt
@@ -1,35 +1,82 @@
 package com.example.flutter_gamepad
 
-import androidx.annotation.NonNull
-
+import android.content.Context
+import android.hardware.input.InputManager
+import android.view.InputDevice
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.EventChannel
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
-/** FlutterGamepadPlugin */
-class FlutterGamepadPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+class FlutterGamepadPlugin :
+  FlutterPlugin,
+  MethodCallHandler,
+  EventChannel.StreamHandler,
+  InputManager.InputDeviceListener {
+  private lateinit var channel: MethodChannel
+  private lateinit var eventChannel: EventChannel
+  private lateinit var inputManager: InputManager
+  private var events: EventChannel.EventSink? = null
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_gamepad")
+  override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+    channel = MethodChannel(binding.binaryMessenger, "com.example.flutter_gamepad/methods")
     channel.setMethodCallHandler(this)
+    eventChannel = EventChannel(binding.binaryMessenger, "com.example.flutter_gamepad/events")
+    eventChannel.setStreamHandler(this)
+    inputManager = binding.applicationContext.getSystemService(Context.INPUT_SERVICE) as InputManager
   }
 
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+  override fun onMethodCall(call: MethodCall, result: Result) {
+    when (call.method) {
+      "gamepads" -> result.success(gamepads())
+      "enableDebugMode", "disableDebugMode" -> result.success(null)
+      else -> result.notImplemented()
     }
   }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
+  override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
+    this.events = events
+    inputManager.registerInputDeviceListener(this, null)
+    gamepads().forEach { gamepad ->
+      events?.success(mapOf("event" to "gamepadConnected", "gamepadId" to gamepad["id"], "gamepadInfo" to gamepad))
+    }
   }
+
+  override fun onCancel(arguments: Any?) {
+    inputManager.unregisterInputDeviceListener(this)
+    events = null
+  }
+
+  override fun onInputDeviceAdded(deviceId: Int) = emitConnection(deviceId, "gamepadConnected")
+  override fun onInputDeviceRemoved(deviceId: Int) = emitConnection(deviceId, "gamepadDisconnected")
+  override fun onInputDeviceChanged(deviceId: Int) = Unit
+
+  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+    onCancel(null)
+    channel.setMethodCallHandler(null)
+    eventChannel.setStreamHandler(null)
+  }
+
+  private fun emitConnection(deviceId: Int, event: String) {
+    val device = InputDevice.getDevice(deviceId) ?: return
+    if (!device.isGamepad) return
+    events?.success(mapOf("event" to event, "gamepadId" to deviceId, "gamepadInfo" to gamepadInfo(device)))
+  }
+
+  private fun gamepads(): List<Map<String, Any>> = InputDevice.getDeviceIds()
+    .mapNotNull(InputDevice::getDevice)
+    .filter(InputDevice::isGamepad)
+    .map(::gamepadInfo)
+
+  private fun gamepadInfo(device: InputDevice): Map<String, Any> = mapOf(
+    "vendorName" to device.name,
+    "productCategory" to "android",
+    "isAttachedToDevice" to false,
+    "id" to device.id,
+  )
+
+  private val InputDevice.isGamepad: Boolean
+    get() = sources and (InputDevice.SOURCE_GAMEPAD or InputDevice.SOURCE_JOYSTICK) != 0
 }
